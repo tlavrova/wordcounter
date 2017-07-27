d4f257dc16973e404dd6edee4a81d9a7eb928ca1
tlavrova <tlavrova230@gmail.com>
2017-07-27 13:06:38 +0200

    formating

--- src/main/java/com/github/tlavrova/wordcounter/CountWords.java
+++ src/main/java/com/github/tlavrova/wordcounter/CountWords.java
@@ -11,31 +11,30 @@ public class CountWords {
 		System.out.println(listOfWords);
 	}
 	
-	public static String getOnlySmallLetters(String str){
+	public static String getOnlySmallLetters(String str) {
 		String newstr = str.replaceAll("[^А-Яа-яA-Za-z]", "");
 		String strLowCase = newstr.toLowerCase();
-		if (strLowCase.isEmpty()){
-			return "";
-				
-		}else{
+		if (strLowCase.isEmpty()) {
+			return "";				
+		} else {
 			return strLowCase;
 		}
 	}
 	
-	public static HashMap<String, Integer> makeWordList() throws FileNotFoundException{
+	public static HashMap<String, Integer> makeWordList() throws FileNotFoundException {
 		File text = new File("C:\\CountWords.txt"); 
 		Scanner scan = new Scanner(text);
 		HashMap<String, Integer> listOfWords = new HashMap<String, Integer>();
 		//Scanner scan1 = new Scanner(scan1);
 		int countWord = 0;
-		while (scan.hasNextLine()){
+		while (scan.hasNextLine()) {
 			String word = scan.next();	//scanner automatically uses " " as a delimeter
 			String wordOnlyLetters = getOnlySmallLetters(word);
-			if (!wordOnlyLetters.isEmpty()){
-				if(!listOfWords.containsKey(wordOnlyLetters)){	//add word if it isn't added already
+			if (!wordOnlyLetters.isEmpty()) {
+				if (!listOfWords.containsKey(wordOnlyLetters)) {	//add word if it isn't added already
 					listOfWords.put(wordOnlyLetters, 1);		//first occurance of this word
 				}
-				else{
+				else {
 					countWord = listOfWords.get(wordOnlyLetters) + 1;	//get current count and increment
 					//now put the new value back in the HashMap
 					listOfWords.remove(wordOnlyLetters);			//first remove it (can't have duplicate keys)
